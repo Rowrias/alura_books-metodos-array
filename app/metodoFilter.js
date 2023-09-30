ee4359c6b217fd338026ca8ba57efd06ace327dd
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-09-30 18:06:55 -0300

    4.1

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -9,9 +9,9 @@
 const botoes = document.querySelectorAll('.btn')
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
-function filtrarLivrosDeFront() {
+function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
-    console.table(livrosFiltrados)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    exibirOsLivrosNaTela(livrosFiltrados)
 }
