a87b6002bb044ce28fcbc1130fd25921d1ce695f
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-09-30 17:32:23 -0300

    3.2

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,7 +1,17 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront)
+// const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
+// btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront)
+
+// function filtrarLivrosDeFront() {
+//     let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+//     console.table(livrosFiltrados)
+// }
+
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivrosDeFront() {
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     console.table(livrosFiltrados)
 }
