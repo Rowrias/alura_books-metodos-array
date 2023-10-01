1dac1cd8ff6fbce8d364a5fcca84a0db2d2fd172
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-10-01 19:27:41 -0300

    5.1

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -12,6 +12,25 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : FiltrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if (categoria == 'disponivel') {
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function FiltrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">199,00</span></p>
+    </div> 
+    `
 }
